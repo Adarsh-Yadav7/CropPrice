023de02d20f7fdae7b348832f0f8ecd192f38c41
Adarsh Yadav <adarshyadav1705@gmail.com>
2025-08-01 19:22:51 +0530

    Update app.py

--- kissan/backend/app.py
+++ kissan/backend/app.py
@@ -6,7 +6,7 @@ from backend.chatbot.api import chatbot_bp
 from backend.weatherapi.api import weather_bp
 from backend.news.api import news_bp
 from backend.pricemodel.api import price_bp
-from backend.crop_recc.api import crop_bp
+
 
 app = Flask(__name__)
 CORS(app)  # ✅ Enable CORS for all routes
@@ -16,13 +16,12 @@ app.register_blueprint(chatbot_bp, url_prefix="/chatbot")
 app.register_blueprint(weather_bp, url_prefix="/weatherapi")
 app.register_blueprint(news_bp, url_prefix="/news")
 app.register_blueprint(price_bp, url_prefix="/pricemodel")
-app.register_blueprint(crop_bp, url_prefix="/crop_recc")
 
 
 # ✅ Optional Root Route
 @app.route("/")
 def home():
-    return {"message": "All APIs (Chatbot, Weather, News, PriceModel, crop) are running!"}
+    return {"message": "All APIs (Chatbot, Weather, News, PriceModel) are running!"}
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
